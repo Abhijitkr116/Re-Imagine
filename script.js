0c07846f6fa6d8883e6b7e4c94783d9205a08ee4
Abhijitkr116 <abhijitkr2003@gmail.com>
2024-04-18 13:53:51 +0530

    Applied responsive nav using JS

--- script.js
+++ script.js
@@ -9,21 +9,35 @@ tl
 })
 .from(".head-p p",{
     x: -50,
+    delay: 0.5,
     opacity: 0,
     duration: 0.7
 })
 .from(".contents h2",{
     // y: 30,
     opacity: 0,
-    duration: 0.8
+    duration: 1
 })
 .from(".foot-p p",{
     x: 50,
     opacity: 0,
     duration: 0.7
 })
-// .from(".cards",{
-//     opacity: 0,
-//     duration: 1,
-//     y: 300
-// })
+
+var icon = document.querySelector(".icon");
+var respoNav = document.querySelector(".responsive-nav");
+var nav = document.querySelector("nav");
+var flag = 0;
+
+icon.addEventListener('click',()=>{
+    if(flag == 0){
+        respoNav.style.left = 0;
+        nav.style.borderBottom = 0;
+        flag = 1;
+    }
+    else if(flag == 1){
+        respoNav.style.left = "-100%";
+        nav.style.borderBottom = "1px solid #222";
+        flag = 0;
+    }
+})
